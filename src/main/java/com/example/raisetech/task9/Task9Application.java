87d067a6de34f71b5312c192faaafe517793ff3e
yuika nagata <xxx.yu.74@gmail.com>
2022-11-04 00:11:41 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/com/example/raisetech/task9/Task9Application.java
@@ -0,0 +1,13 @@
+package com.example.raisetech.task9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/raisetech/task9/Task9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raisetech.task9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
