2704655f78050619b44df42d13dbac9d3be34e86
yuika nagata <xxx.yu.74@gmail.com>
2022-11-08 19:37:20 +0900

    MyBatisを使ったREAD処理の実装

---
+++ src/main/java/com/example/raisetech/task9/Name.java
@@ -0,0 +1,19 @@
+package com.example.raisetech.task9;
+
+public class Name {
+    private String id;
+    private String name;
+
+    public Name(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/example/raisetech/task9/NameMapper.java
@@ -0,0 +1,12 @@
+package com.example.raisetech.task9;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface NameMapper {
+    @Select("SELECT * FROM names")
+    List<Name> findAll();
+}
